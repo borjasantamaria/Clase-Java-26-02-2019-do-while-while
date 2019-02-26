97867575cc3ca2bbe5ae91da3ffeb0337ac5f63a
bsantamaria <borjasantamaria19@gmail.com>
2019-02-26 21:47:24 +0100

    CLASE JAVA 26-02-2019 do-while/while

---
+++ CLASE JAVA 26-02-2019/src/Main.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		/*
+		int contador = 200;
+		
+		//bucle while
+		
+		
+		while (contador<100) { 
+			
+			contador++;
+			
+			System.out.println("CONTADOR: "+contador);
+		}
+		
+		
+		
+		//do while
+		
+		do {
+			
+			contador++;
+			
+			System.out.println("CONTADOR: "+contador);
+			
+			
+		}while (contador<100);
+		
+		
+		
+		int numero;
+		//System.out.print("Introduzca un número entero: ");
+		Scanner teclado = new Scanner(System.in);
+		
+		numero = teclado.nextInt();
+		
+		
+		while(numero != 9) {
+			
+			System.out.println("ESTE NUMERO NO QUIERO");
+			System.out.print("Introduzca un número entero: ");
+			numero = teclado.nextInt();
+			
+			
+		}
+		
+		System.out.println("Este numero si quiero");
+		
+		//dos formas de hacerlo con while y con do while
+		
+		do {
+			
+			System.out.print("Introduzca un número entero: ");
+			numero = teclado.nextInt();
+			
+			
+			
+		}while (numero != 9); System.out.println("Es un nueve");
+			
+		
+			
+		*/
+		
+	
+		
+		
+		
+		
+	}
+
+}
